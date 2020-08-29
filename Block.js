1f6dbcd3c54d9591165161baf260a1e12dc73ec7
Lucca Nunes <luccanunesgit@gmail.com>
2020-08-29 17:18:15 -0300

    added a bunch of methods

--- Block.js
+++ Block.js
@@ -7,4 +7,21 @@ class Block {
         this.h = h;
         this.y = canvas.height - this.h;
     }
+
+    update() {
+        this.x += this.v;
+    }
+
+    hitWall() {
+        return this.x == -5;
+    }
+
+    reverseVel() {
+        this.v *= -1;
+    }
+
+    getNewVel(other) {
+        let newV = (this.v * (this.m - other.m) + 2 * other.m * other.v) / (this.m + other.m);
+        return newV;
+    }
 }
--- script.js
+++ script.js
@@ -4,13 +4,14 @@ let digits = 1;
 let A = new Block(600, 0, 1, 40, 40);
 let M = Math.pow(200, digits - 1);
 let B = new Block(700, -5, M, 80, 80);
+let count = 0;
 
 window.onload = function () {
     ctx = canvas.getContext("2d");
-    setInterval(main, 1000 / 15);
+    interval = setInterval(main, 1000 / 120);
 };
 
-const main = () => {
+const main = async () => {
     ctx.fillStyle = "rgb(218, 218, 218)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -19,4 +20,26 @@ const main = () => {
 
     ctx.fillStyle = "rgb(140, 201, 255)";
     ctx.fillRect(B.x, B.y, B.w, B.h);
+
+    ctx.fillStyle = "red";
+    ctx.fillRect(A.x, A.y, 1, 1);
+    ctx.fillRect(B.x, B.y, 1, 1);
+
+    ctx.fillStyle = "green";
+    ctx.fillRect(A.x + A.w - 1, A.y, 1, 1);
+    ctx.fillRect(B.x + B.w - 1, B.y, 1, 1);
+
+    A.update();
+    B.update();
+
+    if (A.hitWall()) A.reverseVel();
+    if (B.hitWall()) B.reverseVel();
+
+    if (A.x + A.w == B.x + 5 || A.x + 5 == B.x + B.w) {
+        console.log("collided");
+        av = A.getNewVel(B);
+        bv = B.getNewVel(A);
+        A.v = av;
+        B.v = bv;
+    }
 };
