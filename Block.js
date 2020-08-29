572705c34e338e14fb076d2f60cd6a2222aa06bd
Lucca Nunes <luccanunesgit@gmail.com>
2020-08-29 18:20:44 -0300

    Update Block.js

--- Block.js
+++ Block.js
@@ -8,24 +8,19 @@ class Block {
         this.y = canvas.height - this.h;
         this.colour = colour;
     }
-
     show() {
         ctx.fillStyle = this.colour;
         ctx.fillRect(this.x, this.y, this.w, this.h);
     }
-
     update() {
         this.x += this.v;
     }
-
     hitWall() {
         return this.x <= 0;
     }
-
     reverseVel() {
         this.v *= -1;
     }
-
     getNewVel(other) {
         let newV = (this.v * (this.m - other.m) + 2 * other.m * other.v) / (this.m + other.m);
         return newV;
